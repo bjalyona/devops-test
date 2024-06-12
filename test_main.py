a9f7fa2545955b739c16d127b4f76725d23bf464
bjalyona <aly0nasm@mail.ru>
2024-06-12 20:56:38 +0300

    init

---
+++ test_main.py
@@ -0,0 +1,21 @@
+import pytest
+
+from main import plus, minus
+@pytest.mark.parametrize(
+    ('x', 'y', 'result'), [
+        (5, 5, 10),
+        (18, -5, 13),
+    ]
+)
+
+def test_plus(x, y, result):
+    assert plus(x, y) == result
+
+@pytest.mark.parametrize(
+    ('x', 'y', 'result'), [
+        (10, 5, 5),
+        (6, -5, 11),
+    ]
+)
+def test_minus(x, y, result):
+    assert minus(x, y) == result
